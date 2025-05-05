6aad61c559f3208b0388ef0ffb383df0b9a1de3c
OXplakhoa <accbangbangchuot@gmail.com>
2025-05-05 16:11:59 +0700

    Add HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
+import SignInButton from "@/components/SignInButton";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
+  if (session?.user){
+    return redirect("/dashboard")
+  }
   return (
-    <>
-      <h1 className="text-red-600">Hello World</h1>
-      <Button>This is the button</Button>
-    </>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to Quizzis</CardTitle>
+          <CardDescription>
+            {/* TypeWritter here */}
+            Hệ thống ôn thi trắc nghiệm hiệu quả với các bài thi được tạo sẵn bởi AI!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Đăng nhập với Google!"/>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
