aa96cc5a328ad5d16cbd1089c5a59be40ff26aa6
OXplakhoa <accbangbangchuot@gmail.com>
2025-05-05 12:08:29 +0700

    Setup Project & Setup Navbar

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { User } from "next-auth";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  user: Pick<User, "name" | "image">;
+};
+
+const UserAvatar = ({ user }: Props) => {
+  return (
+    <Avatar>
+  {user.image ? (
+    <div className="relative w-10 h-10"> 
+      <Image
+        fill
+        src={user.image}
+        alt="Profile Picture"
+        referrerPolicy="no-referrer"
+        className="object-cover rounded-full"
+      />
+    </div>
+  ) : (
+    <AvatarFallback>
+      <span className="sr-only">{user?.name}</span>
+    </AvatarFallback>
+  )}
+</Avatar>
+
+  );
+};
+
+export default UserAvatar;
